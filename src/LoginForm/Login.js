9b4d88a0e74d940da1773edd58ae0450c3ea8e30
pedronunezcode@gmail.com <pedronunezcode@gmail.com>
2018-10-21 18:43:02 -0700

    Updating the status of the project wortking on a way to not include too much like redux and axios

--- src/LoginForm/Login.js
+++ src/LoginForm/Login.js
@@ -1,44 +1,106 @@
+//Login necessities
 import React, { Component } from 'react'; 
+import $ from 'jquery';
+import TextFieldGroup from '../CommonComponents/TextFieldGroup';
 import './LoginRegister.css';
 
-class Login extends Component {
-  
+//Register necessities
+class LoginAndRegister extends Component {
+  constructor(props) {
+    super(props);
+    this.goRight = this.goRight.bind(this);
+    this.goLeft = this.goLeft.bind(this);
+  }
+
+  goLeft() {
+    $('#slideBox').animate({
+      'marginLeft' : '50%'
+    });
+    $('.topLayer').animate({
+      'marginLeft': '0'
+    });
+  }
+
+  goRight() {
+    $('#slideBox').animate({
+      'marginLeft' : '0'
+    });
+    $('.topLayer').animate({
+      'marginLeft' : '100%'
+    });
+  }
   render() {
+    //Saves the state the errors from the name of name valiable.
+    // name of variable will be set from <div name="Email address"></div>
 
     return (
-      <div >
-        <div>
-          <div className="backRight"></div>
-          <div className="backLeft"></div>
-        </div>
+    // Set background images.
+    <div>
+      <div id="back">
+        <div className="backRight"></div>
+        <div className="backLeft"></div>
+      </div>
 
-        <div id="slideBox">
-          <div className="topLayer">
+      <div id="slideBox">
+        <div className="topLayer">
             <div className="left">
               <div className="content">
                 <h2>Sign Up</h2>
-                <form method="post" onsubmit="return false;">
-                  <div className="form-group">
-                    <input type="text" placeholder="username" />
-                  </div>
-                  <div className="form-group"></div>
-                  <div className="form-group"></div>
-                  <div className="form-group"></div>
+                <form onSubmit={this.onSubmitRegister}>
+                    <TextFieldGroup
+                      type="text"
+                      onChange={this.onChange}
+                      value={this.state.name}
+                      placeholder="Name"
+                      name="name"
+                    />
+                    <TextFieldGroup
+                      type="email"
+                      onChange={this.onChange}
+                      value={this.state.email}
+                      placeholder="Email Address"
+                      name="email"
+                    />
+                    <TextFieldGroup
+                      type="password"
+                      onChange={this.onChange}
+                      value={this.state.password}
+                      placeholder="Password"
+                      name="password"
+                    />
+                    <TextFieldGroup
+                      type="password"
+                      onChange={this.onChange}
+                      value={this.state.value}
+                      placeholder="Confirm new password"
+                      name="password2"
+
+                    />
+                <button onClick={this.goLeft} className="off">Login</button>
+                <button type="submit">Sign up</button>
                 </form>
-                <button id="goLeft" class="off">Login</button>
-                <button>Sign up</button>
               </div>
             </div>
             <div className="right">
               <div className="content">
                 <h2>Login</h2>
-                <form method="post" onsubmit="return false;">
-                  <div className="form-group">
-                    <label for="username" className="form-label">Username</label>
-                    <input type="text" />
-                  </div>
-                  <button id="goRight" className="off">Sign Up</button>
-                  <button id="login" type="submit">Login</button>
+                <form onSubmit={this.onSubmitLogin}>
+                  <TextFieldGroup
+                    placeholder="Email Address"
+                    name="email"
+                    type="email"
+                    value={this.state.email}
+                    onChange={this.onChange}
+                  />
+                  <TextFieldGroup
+                    placeholder="Password"
+                    name="password"
+                    type="password"
+                    value={this.state.password}
+                    onChange={this.onChange}
+                  />
+                <button onClick={this.goRight} type="button" className="off">Sign Up</button>
+                <button id="login">Login</button>
                 </form>
               </div>
             </div>
@@ -49,4 +111,16 @@ class Login extends Component {
   }
 }
 
-export default Login;
+LoginAndRegister.propTypes = {
+  registerUser: PropTypes.func.isRequired,
+  loginUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+export default connect(mapStateToProps, { loginUser, registerUser })(LoginAndRegister);
